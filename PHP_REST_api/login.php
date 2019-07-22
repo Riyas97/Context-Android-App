b8e409f208071d36cc0ea529eedbd3adeeaec558
TCK1997 <achinkhang97.sg@gmail.com>
2019-07-23 01:19:42 +0800

    Added PHP Rest api

---
+++ PHP_REST_api/login.php
@@ -0,0 +1,46 @@
+<?php
+$reply = array();
+include 'db_conn.php'
+
+if(isset($_POST['email']) && isset($_POST['password'])){
+	$email = $_POST['email'];
+    $password = $_POST['password'];
+    
+    $query =
+    "SELECT 
+        user_id, password_hash
+    FROM 
+        users 
+    WHERE 
+        email = ?";
+
+	if($stmt = $conn->prepare($query)){
+		$stmt->bind_param("s",$email);
+		$stmt->execute();
+		$stmt->bind_result($user_id, $password_hash);
+		if($stmt->fetch()){
+			if(password_verify($password,$password_hash)){
+				$response["status"] = 0;
+				$response["message"] = "Login successful";
+				$response["user_id"] = $user_id;
+			}
+			else{
+				$response["status"] = 1;
+				$response["message"] = "Invalid username and password combination";
+			}
+		}
+		else{
+			$response["status"] = 1;
+			$response["message"] = "Invalid username and password combination";
+		}
+		
+		$stmt->close();
+	}
+}
+else{
+	$response["status"] = 2;
+	$response["message"] = "Missing mandatory parameters";
+}
+//Display the JSON response
+echo json_encode($response);
+?>
---
+++ PHP_REST_api/register.php
@@ -0,0 +1,60 @@
+<?php
+
+include 'db_conn.php'
+
+function emailExists($email){
+
+    $query = 
+    "SELECT 
+        email 
+    FROM 
+        users 
+    WHERE 
+        email = ?";
+    
+	global $conn;
+	if($stmt = $conn->prepare($query)){
+		$stmt->bind_param("s",$email);
+		$stmt->execute();
+		$stmt->store_result();
+		$stmt->fetch();
+		if($stmt->num_rows == 1){
+			$stmt->close();
+			return true;
+		}
+		$stmt->close();
+	}
+
+	return false;
+}
+
+$reply = array();
+
+if(isset($_POST['email']) && isset($_POST['password'])){
+	$email = $_POST['email'];
+	$password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+	if(!emailExists($email)){
+		$regquery =
+		"INSERT INTO 
+			users(email, password_hash)
+		VALUES
+			(?,?)";
+		
+		if($stmt = $conn->prepare($regquery)){
+			$stmt->bind_param("ss",$email,$password_hash);
+			$stmt->execute();
+			$reply["status"] = 0;
+			$reply["message"] = "User created";
+			$stmt->close();
+		}
+	} else {
+		$reply["status"] = 1;
+		$reply["message"] = "Email exist in system";
+	}
+} else {
+	$reply["status"] = 2;
+	$reply["message"] = "Missing email or password";
+}
+
+echo json_encode($reply);
+?>
